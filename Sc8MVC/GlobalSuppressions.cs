d741c6ee2e321c472b9dea6bf3cc84c5901717dc
quang.adp <quang.adp@sutrixsolutions.com>
2016-04-19 17:02:18 +0700

    test

---
+++ Sc8MVC/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA2213:Disposable fields should be disposed", Justification = "<Pending>", Scope = "member", Target = "~F:Sc8MVC.DuplicateItems.countryCode")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA2213:Disposable fields should be disposed", Justification = "<Pending>", Scope = "member", Target = "~F:Sc8MVC.DuplicateItems.form1")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA2213:Disposable fields should be disposed", Justification = "<Pending>", Scope = "member", Target = "~F:Sc8MVC.DuplicateItems.hostName")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA2213:Disposable fields should be disposed", Justification = "<Pending>", Scope = "member", Target = "~F:Sc8MVC.DuplicateItems.IsPublish")]
