d121beb22f367bfb45ac1ebdbe080c4baea4de1f
quang.adp <quang.adp@sutrixsolutions.com>
2016-04-27 11:26:52 +0700

    update unit test

--- Sc8Mvc.Test/UnitTest1.cs
+++ Sc8Mvc.Test/UnitTest1.cs
@@ -22,5 +22,13 @@ namespace Sc8Mvc.Test
             double a = item.divide(5, 5);
             Assert.AreEqual(1, a, "test devide message");
         }
+
+        [TestMethod, Timeout(500)]
+        public void TestDothing()
+        {
+            DuplicateItems item = new DuplicateItems();
+            item.doTheThing();
+            Assert.AreEqual(1, 1, "test devide message");
+        }
     }
 }
